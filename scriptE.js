589476fdff21219c336a3a150d2d3e20a4866f69
Minskoleopc <minskoleopc@gmail.com>
2023-04-03 19:47:02 +0530

    added

---
+++ scriptE.js
@@ -0,0 +1,56 @@
+let x =  10
+console.log(x)
+x  = 900
+console.log(x)
+
+// const
+
+const y = 800
+console.log(y)
+//y = 9
+
+
+// Arithmetic operation
+// +,-,*,/,%
+
+let s = 100
+let t = 50
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+
+let a = 8
+let b = 4
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
+// function
+
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+
+Calculator(12,6)
+Calculator(18,9)
+
+
+
+
+
+
+
+
