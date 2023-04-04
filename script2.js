d0baea96550128a1e80f0a4744340e1e4dd7b397
Minskoleopc <minskoleopc@gmail.com>
2023-04-04 09:29:50 +0530

    added

---
+++ script2.js
@@ -0,0 +1,148 @@
+
+// let 
+let x = 10
+console.log(x)
+x = 800
+console.log(x)
+
+const o = 100
+console.log(o)
+//o = 800
+
+// Arithmetic operator
+let x1 = 10
+let y1 = 5
+
+console.log(x1+y1)
+console.log(x1-y1)
+console.log(x1*y1)
+console.log(x1/y1)
+console.log(x1%y1)
+
+
+let s = 8
+let t = 4
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+//console.log(25%6)
+
+
+//-------------------------------------------------
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(15,5)
+Calculator(10,5)
+
+// function without parameter and without return type 
+
+function addA(){
+    console.log(9+9)
+}
+addA()
+addA()
+addA()
+addA()
+
+// function with parameter and without return type
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,1)
+addB(12,3)
+addB(12,5)
+addB(12,8)
+
+// function with parameter and with return type
+// 100 - show
+// 100 - given 
+
+function addC(x,y){
+    return x + y
+}
+let q1 = addC(12,4)
+console.log(q1)
+console.log(q1+5)
+console.log(q1 * 0.10)
+console.log(q1 + q1)
+
+//--------------------------------------------------------------------
+
+// Type 
+
+
+// Introvert     | Extrovert
+// calm          loud
+// less social   more social
+// less outing   more outing
+
+
+// Human
+// Properties - color ,age , weight , height
+// Method     - walk() , talk()
+
+// Vehicle
+// Properties - color , model , type , regNo
+// Methods -- start() , stop()
+
+// Bank 
+// Properties - accNo , accType , accName , type , bal , IFSC
+// Method - withdrawl() , deposit()
+
+
+let q = 10
+console.log(q)
+console.log(typeof q)
+
+//0,12,-12,13.4,14.6
+
+let q11 = true
+console.log(q11)
+console.log(typeof q11)
+// true or false
+
+let q111 = "chinmay"
+console.log(q111)
+console.log(typeof q111)
+// '1' , ' ',"qwerty","A","@@@","!@W123","12312"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
