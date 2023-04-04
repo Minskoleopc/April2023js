288037efc242d4b502ed6d1a0b94c281c7661071
Minskoleopc <minskoleopc@gmail.com>
2023-04-04 19:32:04 +0530

    added

---
+++ script2E.js
@@ -0,0 +1,138 @@
+// let
+// let x = 10
+// console.log(x)
+// x = 100
+// console.log(x)
+
+// // const
+// //const y =  100
+// //y = 9
+
+
+// // arithmetic operation
+
+// let i = 10
+// let q = 5
+
+// console.log(i + q)
+// console.log(i - q)
+// console.log(i * q)
+// console.log(i / q)
+// console.log(i % q)
+
+
+// let n = 100
+// let m = 50
+
+// console.log(n + m)
+// console.log(n - m)
+// console.log(n * m)
+// console.log(n / m)
+// console.log(n % m)
+
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(12,6)
+// Calculator(10,5)
+// Calculator(8,4)
+
+// function without parameter and without return type
+function addA(){
+    console.log(5+5)
+}
+addA()
+addA()
+addA()
+addA()
+addA()
+
+// function with parameter and without  return type 
+function addB(x,y){
+    console.log(x+y)
+}
+addB(10,5)
+addB(12,6)
+addB(120,12)
+
+//--------------------------------------------------------
+
+// function with parameter and with return type 
+// 100 rs shown
+// 100 rs given
+
+function addC(x,y){
+    return x + y
+}
+let q1 = addC(12,3)
+console.log(q1)
+console.log(q1 + q1)
+console.log(q1 * 0.10)
+
+
+// -----------------------------------------------------------------------
+
+
+// Introvert   /  Extrovert 
+// calm            // loud
+// less outing     // more outing
+// less social     // more social
+
+
+// Human 
+// Property -- age , color , weight , height 
+// Method  - walk() , talk()
+
+// Vehicle 
+// Property -  color , type , model ,regNo
+// Method - start() , stop()
+
+// Bank 
+// Propety - accNo , accNo , IFSC code
+// Method - deposit() , withdrawl()
+
+let x = 10
+console.log(x)
+console.log(typeof x)
+// number 8 , -6 , 89.9 ,-90.0,0
+
+let y = true
+console.log(y)
+console.log(typeof y)
+// true or false
+
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+// "A" , "  ","123","@123","213213","321312"
+
+// comparision 
+
+
+// logical operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
