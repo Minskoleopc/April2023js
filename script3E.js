3dd5e7e407bd3403932774626ed46d185e056687
Minskoleopc <minskoleopc@gmail.com>
2023-04-05 19:32:59 +0530

    added

---
+++ script3E.js
@@ -0,0 +1,53 @@
+
+let x = 10
+console.log(x)
+console.log(typeof x)
+// 10,-18,0,89,90 , 90.9, 90.2
+
+
+let y = true
+console.log(y)
+console.log(typeof y)
+
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+//"c" , "12"," ","123","34","123123"
+
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z)
+
+console.log(typeof 10) // number
+console.log(typeof "10") // string
+
+
+// comparision operator 
+//<,> , <= , >= , <= , == , !== , ===
+// == , !=     =====> value
+// === , !==   =====> value and type
+
+console.log(6 == '6')
+console.log(6 === '6')
+console.log(6 !== '6')
+console.log(6 === 6)
+console.log(6 !== 6)
+console.log(6 != 6)
+console.log(6 != '6')
+console.log(6 !== '6')
+console.log(6 > 3)
+console.log(8 < 9)
+console.log(7 >= 6)
+console.log(7 <= 6)
+console.log(6 <= 7)
+console.log(6 <= 6)
+console.log(7 >= 7)
+
+// Logical 
+
+
+
+
+
+
