a3b843157265b983c14f7fe2ef97aede69b48385
Minskoleopc <minskoleopc@gmail.com>
2023-04-05 09:25:12 +0530

    added

---
+++ script3.js
@@ -0,0 +1,79 @@
+
+// type 
+let x = 10
+console.log(x)
+console.log(typeof x)
+// 10 , -10 , 0 , -89, -99 ,89.00.-89.99
+
+
+let y = true
+console.log(y)
+console.log(typeof y)
+// true or false
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+// "c" , "C" ," ","@@123","A123", " ", "A"
+
+
+// comparison operator 
+
+console.log(typeof 10)
+console.log(typeof '10')
+
+// 10 -- number 
+//'10' - string
+
+// < , > , <= , >= , != , == , ===, !==
+// enitity  < entity  ======>    boolean 
+// == , !=   // value
+// === , !== // value and type 
+
+console.log(6 == 6)
+console.log(6 == '6')
+console.log(6 === '6')
+console.log(6 != '6')
+console.log(6 !== '6')
+console.log(6 === 6)
+console.log(6 !== 6)
+console.log(7 != '7')
+console.log(7 == '7')
+console.log(7 == 8)
+console.log(8 === 8)
+console.log(8 !== 8)
+console.log(6 > 3)
+console.log(6 < 3)
+console.log(6 >= 3)
+console.log(6 <= 3)
+console.log(7 <= 7)
+console.log(7 >= 7)
+
+
+// Logical operator 
+
+
+
+// AND   -- &&
+
+
+// true  && true   ===>  true
+// false && true   ===>  false
+// true  && false  ===>  false
+// false && false  ===>  false 
+
+
+// OR   -- ||
+
+// true  || true   ===>  true
+// false || true   ===>  true
+// true  || false  ===>  true
+// false || false  ===>  false 
+
+// not  -  !
+
+//true  --- false
+//false --- true 
+
+
+
