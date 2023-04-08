7ba4c186cbe2a6584699f2f87943594f9e7da427
Minskoleopc <minskoleopc@gmail.com>
2023-04-08 19:23:26 +0530

    added

---
+++ script5E.js
@@ -0,0 +1,121 @@
+
+// one input and multiple outcome
+
+// numT > 0 && numT <= 5   ====> 5 % discount
+// numT > 5 && numT <= 10  ====> 10 % discount
+// numT > 10               ====> 30 % discount
+// conditional statements
+
+
+// if(condition){
+//     // statement
+// }
+// else {
+
+// }
+
+
+// program1
+
+let numT = -1
+// if (numT > 0 && numT <= 5) {
+//     console.log('5 % discount')
+// }
+// if (numT > 5 && numT <= 10) {
+//     console.log('10 % discount')
+// }
+// if (numT > 10) {
+//     console.log('20 % discount')
+// }
+
+// if (numT > 0 && numT <= 5) {
+//     console.log('5 % discount')
+// }
+// else if (numT > 5 && numT <= 10) {
+//     console.log('10 % discount')
+// }
+// else if (numT > 10) {
+//     console.log('20 % discount')
+// }
+// else {
+//     console.log('incorrect input')
+// }
+
+
+
+// program 2
+
+let marks = 62
+
+// if(marks > 90){
+//     console.log("Grade A")
+// }
+// if(marks > 75){
+//     console.log("Grade B")
+// }
+// if(marks > 65){
+//     console.log("Grade C")
+// }
+
+// if(marks > 90){
+//     console.log("Grade A")
+// }
+// else if(marks > 75){
+//     console.log("Grade B")
+// }
+// else if(marks > 65){
+//     console.log("Grade C")
+// }
+// else {
+//     console.log('Fail')
+// }
+
+
+// program 3
+let x = 10
+let y = 50
+
+if(x > y){
+    console.log('x is greater')
+}
+else {
+    console.log('y is greater')
+}
+
+// program 4
+
+let a = 100
+let b = 20
+let c = 30
+
+if(a > b && a > c){
+    console.log('a is greater')
+}
+else if(b > c && b > a){
+    console.log('b is greater')
+}
+else {
+    console.log('c is greater')
+}
+
+
+let s = 90
+let t = 450
+
+// if(s > t){
+//     console.log("s is greater")
+// }
+// else {
+//     console.log("t is greater")
+// }
+
+s > t ? console.log('s is greater'):console.log('t is greater')
+
+
+
+
+
+
+
+
+
