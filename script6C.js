75dd7ae95906b966c47904f19700963542f78fd6
Minskoleopc <minskoleopc@gmail.com>
2023-04-09 09:31:06 +0530

    added

---
+++ script6C.js
@@ -0,0 +1,151 @@
+
+// one input multiple outcome 
+//numT > 0 && numT <= 5  ---- 5 %  discount
+//numT > 5 && numT <= 10 ---- 15 %  discount
+//numT > 10  ----             30 %  discount
+
+let numT = -7
+
+// if(numT > 0 && numT <= 5 ){
+//     console.log("5 %  discount")
+// }
+// if(numT > 5 && numT <= 10 ){
+//     console.log("15 %  discount")
+// }
+// if(numT > 10 ){
+//     console.log("30 %  discount")
+// }
+
+
+// if(numT > 0 && numT <= 5 ){
+//     console.log("5 %  discount")
+// }
+// else if(numT > 5 && numT <= 10 ){
+//     console.log("15 %  discount")
+// }
+// else if(numT > 10 ){
+//     console.log("30 %  discount")
+// }
+// else {
+//     console.log('incorrect input')
+// }
+
+
+let marks = 55
+
+// if(marks > 90){
+//     console.log('Grade A')
+// }
+// if(marks > 75){
+//     console.log('Grade B')
+// }
+// if(marks > 65){
+//     console.log('Grade C')
+// }
+
+// if(marks > 90){
+//     console.log('Grade A')
+// }
+// else if(marks > 75){
+//     console.log('Grade B')
+// }
+// else if(marks > 65){
+//     console.log('Grade C')
+// }
+// else {
+//     console.log('Fail')
+// }
+
+// program 3
+// let a = 10
+// let b = 50
+
+// if(a > b){
+//     console.log('a is greater')
+// }
+// else {
+//     console.log('b is greater')
+// }
+
+// a > b ? console.log('a is greater'):console.log('b is greater')
+// let age = 17
+// let a1 = age >= 18 ? "candrive":"cannot drive"
+// console.log(a1)
+
+
+// Program 4
+// Falsy or Truthy
+
+// Truthy - '1', " ", "chinmay", "$$", [], {} , true , 123 , -123 , 6 = 6
+// undefined , null , 0 , '', NaN , false , 8 !== 8
+
+// if(8 !== 8){
+//     console.log('Hello')
+// }
+// else {
+//     console.log('Bye')
+// }
+
+// program 5 Switch Case
+
+// switch case without break
+
+let city = "FSA"
+// switch(city){
+//     case "pune":
+//         console.log("MH")
+//     case "indore":
+//         console.log("MP")
+//     case "jaipur":
+//         console.log("RJ")
+//     default:
+//         console.log("Incorrect city")
+// }
+
+// switch with break
+// switch(city){
+//     case "pune":
+//         console.log("MH")
+//         break
+//     case "indore":
+//         console.log("MP")
+//         break
+//     case "jaipur":
+//         console.log("RJ")
+//         break
+//     default:
+//         console.log("Incorrect city")
+// }
+
+
+switch(city){
+    case "pune":
+    case "mumbai":
+        console.log("MH")
+        break
+    case "indore":
+    case "bhopal":
+        console.log("MP")
+        break
+    case "jaipur":
+    case "udaipur":
+        console.log("RJ")
+        break
+    default:
+        console.log("Incorrect city")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
