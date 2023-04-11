4f140745ba1246274d87a84de9e2deea98e95d37
Minskoleopc <minskoleopc@gmail.com>
2023-04-11 19:24:24 +0530

    added

---
+++ script7.js
@@ -0,0 +1,84 @@
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+
+// 2 loops
+// for loop and while loop
+
+// for loop 
+
+
+// for(intialization; conditionCheck ; increment/decrement){
+//     // statement1
+//     // statement2
+//     // statement3
+// }
+
+
+for(let i = 0 ; i <= 4 ; i++){ // 1 // 2 //3 // 4 // 5
+    console.log(i) 
+    //1st - i=>0
+    //2nd - i=>1
+    //3rd - i=>2
+    //4rd - i=>3
+    //5th - i=>4
+}
+
+
+// program2 - print hello 3 tyms
+
+for(let i = 0 ; i <= 3 ; i++){  //1 // 2 // 3 // 4
+    console.log("hello")
+    // hello
+    // hello
+    // hello
+    // hello
+}
+
+
+// program2  1 to 5
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 // 3 // 4 // 5
+}
+
+
+// program3 5 to 1
+for(let i = 5 ; i >= 1 ;i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 //1
+}
+
+// program 4
+// table of 2
+
+for(let i = 2 ; i <= 20 ; i = i+2){ // 4 // 6 // 8 // 10 // 12 // 14 // 16 // 18 // 20 //22
+    console.log(i) 
+    // 2
+    // 4
+    // 6
+    // 8
+    // 10
+    // 12
+    // 14
+    //16
+    //18
+    // 20
+
+}
+
+// program 5
+for(let i = 50 ; i>=5 ; i = i- 5){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
