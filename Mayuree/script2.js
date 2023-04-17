34a84d2339530652464b70f390203be0b5d0ec42
Minskoleopc <minskoleopc@gmail.com>
2023-04-17 18:30:47 +0530

    added

---
+++ Mayuree/script2.js
@@ -0,0 +1,114 @@
+
+
+// Type 
+
+//Human
+// Property - weight , height , color gender 
+// Method - talk() walk()
+
+// Vehicle 
+// Properties - type, SUV , color
+// Method - stop() , start()
+
+// Bank - 
+// Property  - accNo , bal , accType , branchCode
+// Method - deposit() , withdrawl()
+
+
+// primitive data types 
+// Number 
+let a = 10
+console.log(a)
+console.log(typeof a)
+
+// Boolean 
+
+let b = true
+console.log(typeof b)
+console.log(b)
+
+
+let firstName = "chinmay"
+console.log(typeof firstName)
+console.log(firstName)
+
+console.log(typeof 10) // number
+console.log(typeof '10') // string 
+
+// comparison operator
+// < , > , <= , >= , != ,== ,!== ,===
+
+// == , !=   ----> value
+// ===, !==  ----> type and value
+
+// console.log(7 === 7) // true
+// console.log(7 !== 7) // false
+// console.log(7 == '8')//false
+// console.log(7 != '7')// false
+// console.log(7 == 7) // true
+// console.log(7 === '7') // false
+// console.log(7 !== '7') // true
+
+// console.log(7 > 9)
+// console.log(8 < 8)
+// console.log(8 < 9)
+// console.log(8 >= 7)
+// console.log(8 <= 7)
+// console.log(9 >= 9)
+// console.log(10 <= 10)
+
+
+// AND  - &&
+//true  && true -----> true
+//false && true -----> false
+//true && false -----> false
+//false && false ----> false
+
+console.log(6 == 6 && 7 == 7)
+console.log(6 != 6 && 7 == 7)
+console.log(6 == 6 && 7 != 7)
+console.log(6 != 6 && 7 != 7)
+
+
+
+
+// OR  - ||
+//true  || true -----> true
+//false || true -----> true
+//true || false -----> true
+//false || false ----> false
+
+
+console.log(6 == 6 || 7 == 7)
+console.log(6 != 6 || 7 == 7)
+console.log(6 == 6 || 7 != 7)
+console.log(6 != 6 || 7 != 7)
+
+
+
+
+
+
+// NOT - !
+
+//false  - true
+//true - false 
+
+console.log(!7 == 7)
+console.log(! 8 != 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
