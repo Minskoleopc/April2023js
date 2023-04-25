6b66313d3006dc39a7f418cd63a3632917fba8f1
Minskoleopc <minskoleopc@gmail.com>
2023-04-25 18:31:08 +0530

    added

---
+++ Mayuree/script6.js
@@ -0,0 +1,79 @@
+
+// switch case without break keword
+let city = "nagpur"
+// switch(city){
+//     case 'pune':
+//         console.log('MH')
+//     case 'jaipur':
+//         console.log('Rajasthan')
+//     case 'bhopal':
+//         console.log('MP')
+//     case 'lucknow':
+//         console.log('Up')
+//     default:
+//         console.log('incorrect city name')
+// }
+
+// program 2
+// switch(city){
+//     case 'pune':
+//         console.log('MH')
+//         break
+//     case 'jaipur':
+//         console.log('Rajasthan')
+//         break
+//     case 'bhopal':
+//         console.log('MP')
+//         break
+//     case 'lucknow':
+//         console.log('Up')
+//         break
+//     default:
+//         console.log('incorrect city name')
+// }
+
+
+// switch(city){
+//     case 'pune':
+//     case 'nagpur':
+//         console.log('MH')
+//         break
+//     case 'jaipur':
+//     case 'udaipur':
+//         console.log('Rajasthan')
+//         break
+//     case 'bhopal':
+//     case 'indore':
+//         console.log('MP')
+//         break
+//     case 'lucknow':
+//     case 'varanasi':
+//         console.log('Up')
+//         break
+//     default:
+//         console.log('incorrect city name')
+// }
+    
+
+//Falsy ==>  0 , null, NaN, undefined , '', 5 > 6 , false 
+// Truthy value ==> true , 123,-123,'**',[], {} ,true 8 === 8
+
+if(8===8){
+    console.log('hello')
+}
+else {
+    console.log('bye')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
