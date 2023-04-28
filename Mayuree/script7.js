fcdcdf62d3eca1e2bd2fb05e50b42b064ce9e6e4
Minskoleopc <minskoleopc@gmail.com>
2023-04-28 18:24:57 +0530

    added

---
+++ Mayuree/script7.js
@@ -0,0 +1,67 @@
+//loops 
+
+// program 1
+// print 1 to 5
+
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// // Loops (for loop  and while loop)
+
+// for(intialization ; conditionCheck; increment/Decrement){
+//     // statements
+// }
+
+// program 1
+for(let i = 1 ; i <= 3 ; i++){ //2 // 3 // 4
+    console.log(i) // 1 /// 2 // 3
+}
+
+// program 2
+// print "hello"  3 times 
+
+for(let i = 1 ; i <= 3 ; i++){
+    console.log("hello")
+}
+
+// print 1 to 5
+for(let i = 1 ; i <= 5 ; i++){ //2 // 3 // 4 // 5 // 6
+    console.log(i) //1 // 2 //3 // 4 // 5
+}
+
+// print 5 to 1
+for(let i = 5 ; i >= 1 ; i--){  // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5  // 4 // 3 // 2 // 1
+}
+
+
+// table of 2 
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 // 6 // 8 // 10 ----// 22
+    console.log(i) // 2 // 4 // 6 // 8 // 10 ------- 20
+}
+
+//table of 3 in reverse
+
+for(let i = 30 ; i >= 3 ; i = i - 3){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
